b0a60d5b0fc9bd45385603a8e66296a9dfd14da3
MassimilianoTusini <massimilianotusini110@gmail.com>
2025-10-29 16:11:49 +0100

    Crea la rotta SHOW per mostrare il signolo post tramite ID

--- server.js
+++ server.js
@@ -15,3 +15,20 @@ app.get('/posts', (req, res) => {
         res.json(results);
     });
 });
+
+// SHOW per mostrare un post tramite ID
+app.get('/posts:id', (req, res) => {
+    const id = req.params.id;
+    const sql = 'SELECT * FROM posts WHERE id = ?';
+
+    connection.query(sql, [id], (err, results) => {
+        if (err) {
+            console.error('Errore nel recupero del post:', err);
+            return res.status(500).json({ error: 'Errore nel recupero del post:', err});
+        }
+        if (results.length === 0) {
+            return res.status(404).json({error: 'Post non trovato'});
+        }
+        res.json(results[0]);
+    });
+});
